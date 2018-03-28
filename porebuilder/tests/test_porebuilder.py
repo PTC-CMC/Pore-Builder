e6aba0422f6c428c8ea3bc0a74fc42ff4d14e746
Ray Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2018-03-28 11:49:54 -0500

    Add 'test_save' and 'test_porewidth' unittests

--- porebuilder/tests/test_porebuilder.py
+++ porebuilder/tests/test_porebuilder.py
@@ -1,8 +1,20 @@
 import numpy as np
 import pytest
 import mbuild as mb
-import mbuild.tests.base_test import BaseTest
+import base_test import BaseTest
 
 class TestPoreBuilder(BaseTest):
+    """
+    Unit Tests for Pore class functionality.
+    """
 
-    def test_porewidth(self, 
+    def test_save(self, gph_pore_solv):
+        gph_pore_solv.save(filename='gph_pore.gro')
+
+    def test_porewidth(self, gph_pore_solv):
+        pore = gph_pore_solv() # may need to correct
+        bot = np.min(pore.bottom_sheet.xyz[:,1])
+        top = np.max(pore.top_sheet.xyz[:,1]) # change naming
+        pore_width = bot - top
+        np.testing.assert_almost_equal(pore_width, 1, 4) 
+        # may have to change rounding precision
