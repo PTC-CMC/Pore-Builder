b03123813900a58293a52b416d9058542be58e46
mattwthompson <matt.thompson@vanderbilt.edu>
2018-04-06 13:19:25 -0500

    Load inputs from a source within the repo

--- porebuilder/examples/example.py
+++ porebuilder/examples/example.py
@@ -1,16 +1,17 @@
-from porebuilder import gph_pore_solv
+from porebuilder import GraphenePoreSolvent
 import mbuild as mb
 import numpy as np
 import foyer
 from foyer import Forcefield
 import parmed as pmd
 
-water = '/Users/raymatsumoto/science/il_solvent_local/file_gen/mol2/tip3p.mol2'
-ch3cn = '/Users/raymatsumoto/science/il_solvent_local/file_gen/mol2/ch3cn.mol2'
-system = gph_pore_solv(x_sheet=4, y_sheet=4, sheets=3, pore_width=1.2,
-        x_bulk=3, solvent=[{'SOL': water}, {'ch3cn':ch3cn}], n_solvent=[1000,300])
-C_spce = 'C-spce.xml'
-C_spce = Forcefield(C_spce)
+water = 'files/tip3p.mol2'
+acn = 'files/acn.mol2'
+
+system = GraphenePoreSolvent(x_sheet=4, y_sheet=4, sheets=3, pore_width=1.2,
+        x_bulk=3, solvent=[{'SOL': water}, {'acn':acn}], n_solvent=[1000,300])
+
+C_spce = Forcefield('files/C-spce.xml')
 opls = Forcefield(name='oplsaa')
 
 box = mb.Box(system.box)
@@ -19,20 +20,20 @@ for child in system.children:
     if child.name in 'Compound':
         totalPM += child.to_parmed(residues='Compound', box=box)
     elif child.name in system.fluid_name[1]:
-        totalPM += child.to_parmed(residues='ch3cn', box=box)
+        totalPM += child.to_parmed(residues='acn', box=box)
     elif child.name in system.fluid_name[0]:
         totalPM += child.to_parmed(residues='SOL', box =box)
 
-ch3cnPM = totalPM['ch3cn',:]
+acnPM = totalPM['acn',:]
 SOLPM = totalPM['SOL',:]
 gphPM = totalPM['Compound',:]
 import pdb; pdb.set_trace()
 
 SOLPM = C_spce.apply(SOLPM, residues='SOL')
 gphPM = C_spce.apply(gphPM, residues='Compound')
-ch3cnPM = opls.apply(ch3cnPM, residues='ch3cn')
+acnPM = opls.apply(acnPM, residues='acn')
 
-systemPM = SOLPM + gphPM + ch3cnPM 
+systemPM = SOLPM + gphPM + acnPM
 systemPM.box = np.empty(6)
 systemPM.box[:3] = box.maxs * 10 # convert from nm to angstroms
 systemPM.box[3:7] = 90
