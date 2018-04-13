e197f79f61fef995b6c3491c2bc97fe20fea0258
mattwthompson <matt.thompson@vanderbilt.edu>
2018-04-12 23:48:19 -0500

    Clean up xml files and use

--- porebuilder/examples/solvated_pore.py
+++ porebuilder/examples/solvated_pore.py
@@ -6,33 +6,36 @@ from foyer import Forcefield
 import parmed as pmd
 
 water = mb.load('files/tip3p.mol2')
+water.name = 'SOL'
 acn = mb.load('files/acn.mol2')
+acn.name = 'acn'
 
-C_spce = Forcefield('files/C-spce.xml')
-opls = Forcefield(name='oplsaa')
+C = Forcefield('files/carbon.xml')
+SPCE = Forcefield('files/spce.xml')
+OPLS = Forcefield(name='oplsaa')
 
 system = GraphenePoreSolvent(x_sheet=4, y_sheet=4, sheets=3, pore_width=1.2,
-        x_bulk=3, solvent=[{'SOL': water}, {'acn':acn}], n_solvent=[1000,300])
+        x_bulk=3, solvent=[water, acn], #[{'SOL': water}, {'acn':acn}],
+        n_solvent=[1, 1])
 
-
-box = mb.Box(system.box)
+box = mb.Box(system.periodicity)
 system_pmd = pmd.Structure()
 
 for child in system.children:
-    if child.name in 'Compound':
-        system_pmd += child.to_parmed(residues='Compound', box=box)
-    elif child.name in system.fluid_name[1]:
+    if child.name in 'GraphenePore':
+        system_pmd += child.to_parmed(residues='GraphenePore', box=box)
+    elif child.name in 'acn':
         system_pmd += child.to_parmed(residues='acn', box=box)
-    elif child.name in system.fluid_name[0]:
+    elif child.name in 'SOL':
         system_pmd += child.to_parmed(residues='SOL', box =box)
 
 acn_pmd = system_pmd['acn',:]
 SOL_pmd = system_pmd['SOL',:]
-gph_pmd = system_pmd['Compound',:]
+gph_pmd = system_pmd['GraphenePore',:]
 
-SOL_pmd = C_spce.apply(SOL_pmd, residues='SOL')
-gph_pmd = C_spce.apply(gph_pmd, residues='Compound')
-acn_pmd = opls.apply(acn_pmd, residues='acn')
+SOL_pmd = SPCE.apply(SOL_pmd)#, residues='SOL')
+gph_pmd = C.apply(gph_pmd)#, residues='GraphenePore')
+acn_pmd = OPLS.apply(acn_pmd)#, residues='acn')
 
 system_pmd = SOL_pmd + gph_pmd + acn_pmd
 system_pmd.box = np.empty(6)
