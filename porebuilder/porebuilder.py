23a0ad9b3271e86a762b0f1ea5b087d16af7cb7f
mattwthompson <matt.thompson@vanderbilt.edu>
2018-04-20 12:07:30 -0500

    Pre-save some inputs as variables

--- porebuilder/porebuilder.py
+++ porebuilder/porebuilder.py
@@ -92,11 +92,16 @@ class GraphenePore(mb.Compound):
         carbon.name = 'C'
         carbon_locations = [[0, 0, 0], [2/3, 1/3, 0]]
         basis = {carbon.name: carbon_locations}
-        graphene_lattice = mb.Lattice(lattice_spacing=[0.2456, 0.2456, 0.335],
-                                      angles=[90, 90, 120], lattice_points=basis)
+        lattice_spacing = [0.2456, 0.2456, 0.335]
+        angles = [90.0, 90.0, 120.0]
+
+        graphene_lattice = mb.Lattice(lattice_spacing=lattice_spacing,
+                                      angles=angles, lattice_points=basis)
+
         graphene = graphene_lattice.populate(compound_dict={carbon.name: carbon},
                                              x=replicate[0], y=replicate[1],
                                              z=sheets)
+
         for particle in graphene.particles():
             if particle.xyz[0][0] < 0:
                 particle.xyz[0][0] += graphene.periodicity[0]
