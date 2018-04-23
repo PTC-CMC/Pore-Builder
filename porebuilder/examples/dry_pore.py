3204d0b6c1edb777430b80e725a6c634187d3cd9
mattwthompson <matt.thompson@vanderbilt.edu>
2018-04-23 15:04:39 -0500

    Update examples

--- porebuilder/examples/dry_pore.py
+++ porebuilder/examples/dry_pore.py
@@ -1,20 +1,11 @@
 from porebuilder import GraphenePore
-import mbuild as mb
-import numpy as np
-import foyer
 from foyer import Forcefield
-import parmed as pmd
 
 C = Forcefield('files/carbon.xml')
 
-system = GraphenePore(x_sheet=3, y_sheet=3, sheets=3, pore_width=1.2)
+system = GraphenePore(x_sheet=4, y_sheet=2, sheets=3, pore_width=1.2)
 
-box = mb.Box(system.periodicity)
+system = C.apply(system)
 
-system_pmd = system.to_parmed(residues=['Compound'])
-
-system_pmd.box = np.empty(6)
-system_pmd.box[:3] = box.maxs * 10 # convert from nm to angstroms
-system_pmd.box[3:7] = 90
-system_pmd.save('init.gro', overwrite=True)
-system_pmd.save('init.top', overwrite=True)
+system.save('dry_pore.gro', overwrite=True)
+system.save('dry_pore.top', overwrite=True, combine='all')
--- porebuilder/examples/solvated_pore.py
+++ porebuilder/examples/solvated_pore.py
@@ -1,12 +1,9 @@
 from porebuilder import GraphenePoreSolvent
 import mbuild as mb
-import numpy as np
-import foyer
 from foyer import Forcefield
-import parmed as pmd
 
 water = mb.load('files/tip3p.mol2')
-water.name = 'SOL'
+water.name = 'water'
 acn = mb.load('files/acn.mol2')
 acn.name = 'acn'
 
@@ -19,27 +16,16 @@ system = GraphenePoreSolvent(x_sheet=4, y_sheet=4, sheets=3, pore_width=1.2,
         n_solvent=[1, 1])
 
 box = mb.Box(system.periodicity)
-system_pmd = pmd.Structure()
 
 for child in system.children:
-    if child.name in 'GraphenePore':
-        system_pmd += child.to_parmed(residues='GraphenePore', box=box)
-    elif child.name in 'acn':
-        system_pmd += child.to_parmed(residues='acn', box=box)
-    elif child.name in 'SOL':
-        system_pmd += child.to_parmed(residues='SOL', box =box)
+    if child.name == 'water':
+        water_pmd = SPCE.apply(child)
+    elif child.name == 'acn':
+        acn_pmd = OPLS.apply(child)
+    else:
+        pore_pmd = C.apply(child)
 
-acn_pmd = system_pmd['acn',:]
-SOL_pmd = system_pmd['SOL',:]
-gph_pmd = system_pmd['GraphenePore',:]
-
-SOL_pmd = SPCE.apply(SOL_pmd)#, residues='SOL')
-gph_pmd = C.apply(gph_pmd)#, residues='GraphenePore')
-acn_pmd = OPLS.apply(acn_pmd)#, residues='acn')
-
-system_pmd = SOL_pmd + gph_pmd + acn_pmd
-system_pmd.box = np.empty(6)
-system_pmd.box[:3] = box.maxs * 10 # convert from nm to angstroms
-system_pmd.box[3:7] = 90
-system_pmd.save('init.gro', overwrite=True)
-system_pmd.save('init.top', overwrite=True)
+system = water_pmd + acn_pmd + pore_pmd
+system.box[:3] = box.maxs * 10.0
+system.save('solvated_pore.gro', overwrite=True)
+system.save('solvated_pore.top', overwrite=True, combine='all')
