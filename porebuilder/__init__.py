dcefc78e14d58469551bf8c27197f0a0ea6a3389
mattwthompson <matt.thompson@vanderbilt.edu>
2018-05-03 09:57:15 -0500

    Remove support for py27, py34, py36
    https://travis-ci.org/mattwthompson/Pore-Builder/builds/374448188
    
    Python 2.7 was erroring for unclear reasons
    Python 3.4 was erroring because of a missing mbuild binary
    (see https://github.com/mosdef-hub/mbuild/issues/424)
    Python 3.6 ran into a plyplus dependency issue

--- porebuilder/__init__.py
+++ porebuilder/__init__.py
@@ -1,2 +1,2 @@
-from .porebuilder import GraphenePoreSolvent
-from .porebuilder import GraphenePore
+from porebuilder.porebuilder import GraphenePoreSolvent
+from porebuilder.porebuilder import GraphenePore
