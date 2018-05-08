75cc540686d914a11bcd31614d18d6cca3ee9e0c
mattwthompson <matt.thompson@vanderbilt.edu>
2018-05-08 12:43:00 -0500

    Remove testing on py34 again

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
 
